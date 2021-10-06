aa9c4be6c83d7e4fea0c21d14c9cd50a6bf8e5fd
leochastagnac <leochastagnac@gmail.com>
2021-10-06 17:10:55 +0200

    VueJS

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new Vue ({
+    el: '#app',
+    data: {
+        message : 'hello',
+        link : 'https://leochastagnac.fr',
+        valeur : 'mon site',
+        test : 'test',
+        awesome : false
+    }
+})
